9ce3f391c32c13724955d74dff272da6d171ce41
garzo94 <alexgarzo25@gmail.com>
2022-09-28 06:58:32 -0600

    my projects

--- apiNotes/settings.py
+++ apiNotes/settings.py
@@ -52,12 +52,7 @@ INSTALLED_APPS = [
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'alexgarzo25@gmail.com'
-EMAIL_HOST_PASSWORD = 'vuiwtsttnvbblfsb'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+
 
 
 DJOSER ={
